92442297caf3d3dd6ab7e22f34c6e20bd259f691
Bruno S Velho <bruno.velho@gmail.com>
2023-10-08 15:02:14 -0300

    Add migration na app

---
+++ src/database/knex/migrations/20231008160904_createUsers.js
@@ -0,0 +1,13 @@
+exports.up = (knex) => knex.schema.createTable('users', (table) => {
+  table.increments('id');
+
+  table.string('name');
+  table.string('email');
+  table.string('password');
+  table.string('avatar');
+
+  table.timestamp('created_at').default(knex.fn.now());
+  table.timestamp('update_at').default(knex.fn.now());
+});
+
+exports.down = (knex) => knex.schema.dropTable('users');
